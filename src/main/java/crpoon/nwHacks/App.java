7ae213163c3c00acbef52e07275d2899ecee74b6
Christopher Poon <crpoon@gmail.com>
2017-03-18 13:21:28 -0700

    Initial Commit with Maven project

---
+++ src/main/java/crpoon/nwHacks/App.java
@@ -0,0 +1,13 @@
+package crpoon.nwHacks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

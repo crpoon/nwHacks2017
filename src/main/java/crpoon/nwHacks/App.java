a8e9f28d6487b590494df8b1c8750c7927017317
dj-wong <darrenwongj@gmail.com>
2017-03-19 00:59:10 -0700

    added InstagramClient.java

--- src/main/java/crpoon/nwHacks/App.java
+++ src/main/java/crpoon/nwHacks/App.java
@@ -12,9 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        TwitterService.getInstance().initializeTwitterStockInfo();
-        System.out.println("YAAAAAAS");
+        //System.out.println( "Hello World!" );
+        //TwitterService.getInstance().initializeTwitterStockInfo();
+        //System.out.println("YAAAAAAS");
         //System.out.println(TwitterClient.getInstance().getTweets("#fakenews"));
+        InstagramClient.getInstance().getInstasTags("selfies");
     }
 }

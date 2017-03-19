21f57ddf7309d3b8bc6622bb14378cbedea7619d
djwong <darren.wong@incognito.com>
2017-03-18 20:24:06 -0700

    initial commit for styling

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Chart from "./chart"
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css'
 import './index.css';
 
 var options = {

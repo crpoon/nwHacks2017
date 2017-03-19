ae3334f6c032955e18775731286702ac240b1bc8
djwong <darren.wong@incognito.com>
2017-03-19 12:37:55 -0700

    final commit

--- webapp/src/tabModule.js
+++ webapp/src/tabModule.js
@@ -36,7 +36,7 @@ class TabModule {
             },
             yAxis: {
                 opposite: true,
-                title: null
+                title: "Rare Pepes (RP)"
             },
             legend: {
                 floating: true,
@@ -45,7 +45,7 @@ class TabModule {
                 align: 'left',
                 verticalAlign: 'top',
                 labelFormatter: function() {
-                    return "<div class'legend-label'><span>" + this.options.displayName + "</span><div class='pepecoin'></div></div>";
+                    return "<div id='" + this.options.name + "'class='legend-label " + tabId + "'><span id='current-price'></span><span id='difference'></span><div class='pepecoin'></div><span>" + this.options.displayName + "</span></div>";
                 }
             },
 
@@ -57,7 +57,7 @@ class TabModule {
         };
 
         if(tabId === "twitch") {
-            var startDate = new Date("2017-01-15T12:00:00.000Z");
+            var startDate = new Date("2017-01-17T00:00:00.000Z");
             var endDate = new Date("2017-01-19T00:00:00.000Z");
             config.xAxis.min = startDate.valueOf();
             config.xAxis.max = endDate.valueOf();
@@ -118,12 +118,32 @@ class TabModule {
                                                 x: date.valueOf(),
                                                 y: responseData[i].price
                                             }
-                                            var shift = series.data.length > 36;
+
+                                            var legend;
+
+                                            var legendList = document.getElementsByClassName("legend-label "+ tabId);
+
+                                            for(var k = 0; k < legendList.length; k++) {
+                                                if(legendList[k].id === name) {
+                                                    legend = legendList[k];
+                                                    break;
+                                                }
+                                            }
+                                            legend.children[0].innerHTML = responseData[i].price.toFixed(2);
+                                            legend.children[1].innerHTML = " (" + responseData[i].increase.toFixed(2) + ") ";
+                                            if(responseData[i].increase >= 0) {
+                                                legend.children[1].className = "increase";
+                                            } else {
+                                                legend.children[1].className = "decrease";
+                                            }
+
+                                            console.log(tabId);
+                                            var shift = ((series.data) ? series.data.length : 0) > 240;
                                             series.addPoint(newPoint, true, shift);
 
                                         }
                                     })
-                            }, 30000)
+                            }, 15000)
                         }
 
       }

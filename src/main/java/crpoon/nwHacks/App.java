851faf4ea2bcf47a656686a3b122be2c3241edef
Christopher Poon <crpoon@gmail.com>
2017-03-18 23:12:11 -0700

    Initialize Twitter stocking db

--- src/main/java/crpoon/nwHacks/App.java
+++ src/main/java/crpoon/nwHacks/App.java
@@ -1,6 +1,7 @@
 package crpoon.nwHacks;
 
 import crpoon.nwHacks.client.TwitterClient;
+import crpoon.nwHacks.service.TwitterService;
 
 /**
  * Hello world!
@@ -11,6 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(TwitterClient.getInstance().getTweets("#fakenews"));
+        TwitterService.getInstance().initializeTwitterStockInfo();
+        System.out.println("YAAAAAAS");
+        //System.out.println(TwitterClient.getInstance().getTweets("#fakenews"));
     }
 }
--- src/main/java/crpoon/nwHacks/service/TwitterService.java
+++ src/main/java/crpoon/nwHacks/service/TwitterService.java
@@ -1,7 +1,38 @@
 package crpoon.nwHacks.service;
 
 
+import com.google.common.collect.ImmutableList;
+import crpoon.nwHacks.database.StockInfoDao;
+import crpoon.nwHacks.model.StockInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TwitterService {
 
-    //public
+    private static TwitterService instance;
+
+    private TwitterService() {
+    }
+
+    public static TwitterService getInstance() {
+        if (instance == null) {
+            instance = new TwitterService();
+        }
+        return instance;
+    }
+
+    public void initializeTwitterStockInfo() {
+        List<String> twitterSector = ImmutableList.of("twitter");
+
+        StockInfo trump = new StockInfo("Trump", "TRMP", twitterSector, "");
+        StockInfo fakeNews = new StockInfo("Fake News", "FKNW", twitterSector, "");
+        StockInfo saltBae = new StockInfo("Salt Bae", "SALT", twitterSector, "");
+        StockInfo win = new StockInfo("#win", "WIN", twitterSector, "");
+
+        List<StockInfo> infos = ImmutableList.of(trump, fakeNews, saltBae, win);
+        for (StockInfo info : infos) {
+            StockInfoDao.getInstance().insertStockInfo(info);
+        }
+    }
 }

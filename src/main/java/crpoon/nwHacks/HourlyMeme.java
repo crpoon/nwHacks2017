fdc97db19a488c24a675732e63fb30acc0a5b153
dj-wong <darrenwongj@gmail.com>
2017-03-19 03:46:32 -0700

    Preliminary commit of chatlog parser.

---
+++ src/main/java/crpoon/nwHacks/HourlyMeme.java
@@ -0,0 +1,27 @@
+package crpoon.nwHacks;
+
+import java.util.Date;
+
+public class HourlyMeme {
+	private Date hour;
+	private String meme;
+	private int count;
+	
+	public HourlyMeme(Date hour, String meme, int count){
+		this.hour = hour;
+		this.meme = meme;
+		this.count = count;
+	}
+	
+	public Date getDate(){
+		return this.hour;
+	}
+	
+	public String getMeme(){
+		return this.meme;
+	}
+	
+	public int getCount(){
+		return this.count;
+	}
+}

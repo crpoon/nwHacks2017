0d169733a2e242a222eeb29cbb43c64caf87c6f3
dj-wong <darrenwongj@gmail.com>
2017-03-19 06:09:27 -0700

    Added InstagramInfoDao and InstagramInfo classes.

--- src/main/java/crpoon/nwHacks/App.java
+++ src/main/java/crpoon/nwHacks/App.java
@@ -2,6 +2,7 @@ package crpoon.nwHacks;
 
 import crpoon.nwHacks.client.InstagramClient;
 import crpoon.nwHacks.client.TwitterClient;
+import crpoon.nwHacks.service.InstagramService;
 import crpoon.nwHacks.service.TwitterService;
 
 /**
@@ -13,12 +14,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
         TwitterService.getInstance().initializeTwitterStockInfo();
+        InstagramService.getInstance().initializeInstagramStockInfo();
+
         System.out.println("YAAAAAAS");
+
         TwitterService ts = TwitterService.getInstance();
+        InstagramService is = InstagramService.getInstance();
+
         ts.calculateAndPersistStock("Fake News");
+        is.calculateAndPersistStock("Selfies");
         System.out.println("YAAAAAASssssss");
 
-        InstagramClient.getInstance().getInstasTags("selfies");
+        //InstagramClient.getInstance().getInstasTags("selfies");
     }
 }

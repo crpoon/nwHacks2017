20b45a328459b8735876f0498ec691cc49f8f9f8
dj-wong <darrenwongj@gmail.com>
2017-03-19 04:14:27 -0700

    Probably finished service

--- src/main/java/crpoon/nwHacks/service/Service.java
+++ src/main/java/crpoon/nwHacks/service/Service.java
@@ -1,7 +1,9 @@
 package crpoon.nwHacks.service;
 
 import crpoon.nwHacks.database.StockDao;
+import crpoon.nwHacks.database.StockInfoDao;
 import crpoon.nwHacks.model.Stock;
+import crpoon.nwHacks.model.StockInfo;
 
 import java.util.Collections;
 import java.util.Date;
@@ -26,26 +28,46 @@ public abstract class Service {
             sumIncrease += stock.getCurIncrease();
         }
         double averageIncrease = sumIncrease / stocks.size();
-
         double multiplier = (MAX_COUNT - 1) / MAX_COUNT;
-
         return multiplier * averageIncrease;
     }
 
-    public double getCurrentIncreasePerHour(String hashtag) {
-        int count = getCurrentIncrease(hashtag);
-
-        List<Stock> stocks = StockDao.getInstance().getAllStockByName(hashtag);
-        if (stocks == null || stocks.isEmpty()) {
+    public double getCurrentIncreasePerHour(String hashtag, Stock lastStock) {
+        if (lastStock == null) {
             return 100.0;
         }
-        Stock lastStock = stocks.get(0);
+        int count = getCurrentIncrease(hashtag);
         Date lastStockDate = lastStock.getDate();
         double diffTime = System.currentTimeMillis() - lastStockDate.getTime();
         diffTime = ONE_HOUR / diffTime;
 
         double multiplier = 1 / MAX_COUNT;
         return multiplier * count * diffTime;
+    }
+
+    public void calculateAndPersistStock(String hashtag) {
+        StockInfo info = StockInfoDao.getInstance().getStockInfoByName(hashtag);
+        String ticker = info.getTicker();
+
+        double currentPrice;
+        double currentIncrease;
+        List<Stock> stocks = StockDao.getInstance().getAllStockByName(hashtag);
+        if (stocks == null || stocks.isEmpty()) {
+            currentPrice = 100.0;
+            currentIncrease = 0.0;
+        } else {
+            Stock lastStock = stocks.get(0);
+            double currentIncreasePerHour = getCurrentIncreasePerHour(hashtag, lastStock);
+            currentPrice = calculatePrice(getAverageIncomePerHour(hashtag),
+                    currentIncreasePerHour, lastStock);
+            currentIncrease = currentPrice - lastStock.getPrice();
+        }
+        Stock stock = new Stock(hashtag, ticker, new Date(), currentPrice, currentIncrease);
+        StockDao.getInstance().insertStock(stock);
+    }
+
+    private double calculatePrice(double avgIncPerHour, double curIncPerHour, Stock lastStock) {
+        return lastStock.getPrice() + (curIncPerHour - avgIncPerHour);
     }
 
     public abstract int getCurrentIncrease(String hashtag);
